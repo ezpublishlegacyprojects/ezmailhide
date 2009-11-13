c929ccf5adcbadc4aadb172764878833a6df7166
Ronan Guilloux <info@example.com>
2009-11-13 11:30:46 +0000

    git-svn-id: http://svn.projects.ez.no/ezmailhide@1 a06c7e6c-dce2-47f1-ba10-c7e755d02bcc

---
+++ autoloads/eztemplateautoload.php
@@ -0,0 +1,10 @@
+<?php
+
+$eZTemplateOperatorArray = array();
+
+$eZTemplateOperatorArray[] = array( 'script' => 'extension/ezmailhide/autoloads/ezmailhideutils.php',
+                                    'class' => 'eZMailHideUtils',
+                                    'operator_names' => array( 'ezmailhide' ) );
+
+
+?>
